0ac185d93b88b3e0dd0cdf6c5136a54ccd9e6270
Sean <sean@organgrindermedia.com>
2022-12-22 10:11:09 -0500

    fix(wip): added some structs

--- programs/multisig/src/lib.rs
+++ programs/multisig/src/lib.rs
@@ -13,3 +13,84 @@ pub mod multisig {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+
+#[account]
+pub struct Ms {
+    pub threshold: u16,                 // threshold for signatures
+    pub authority_index: u16,           // index to seed other authorities under this multisig
+    pub transaction_index: u32,         // look up and seed reference for transactions
+    pub ms_change_index: u32,           // the last executed/closed transaction
+    pub bump: u8,                       // bump for the multisig seed
+    pub create_key: Pubkey,             // random key(or not) used to seed the multisig pda
+    pub allow_external_execute: bool,   // allow non-member keys to execute txs
+    pub keys: Vec<Pubkey>,              // keys of the members
+    pub config_authority: Pubkey               // the external multisig authority
+}
+
+#[derive(Accounts)]
+pub struct MsAuth<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"squad",
+            multisig.create_key.as_ref(),
+            b"multisig"
+        ], bump = multisig.bump,
+        constraint = multisig.external_authority == external_authority.key() @GraphsError::InvalidExternalAuthority
+    )]
+    multisig: Box<Account<'info, Ms>>,
+    #[account(mut)]
+    pub external_authority: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct MsAuthRealloc<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"squad",
+            multisig.create_key.as_ref(),
+            b"multisig"
+        ], bump = multisig.bump,
+        constraint = multisig.external_authority == external_authority.key() @GraphsError::InvalidExternalAuthority
+    )]
+    multisig: Box<Account<'info, Ms>>,
+    #[account(mut)]
+    pub external_authority: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(external_authority: Pubkey, threshold: u16, create_key: Pubkey, members: Vec<Pubkey>)]
+pub struct Create<'info> {
+    #[account(
+        init,
+        payer = creator,
+        space = Ms::SIZE_WITHOUT_MEMBERS + (members.len() * 32),
+        seeds = [b"squad", create_key.as_ref(), b"multisig"], bump
+    )]
+    pub multisig: Account<'info, Ms>,
+
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(config_authority: Pubkey, threshold: u16, create_key: Pubkey, members: Vec<Pubkey>)]
+pub struct Create<'info> {
+    #[account(
+        init,
+        payer = creator,
+        space = Ms::SIZE_WITHOUT_MEMBERS + (members.len() * 32),
+        seeds = [b"squad", create_key.as_ref(), b"multisig"], bump
+    )]
+    pub multisig: Account<'info, Ms>,
+
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
