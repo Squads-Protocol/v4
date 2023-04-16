49b29b50613182c5dd7de5a391de03729a4bba49
Vladimir Guguiev <1524432+vovacodes@users.noreply.github.com>
2023-04-16 09:50:44 +0200

    chore(ci): run tests in CI

--- sdk/multisig/.solitarc.js
+++ sdk/multisig/.solitarc.js
@@ -4,7 +4,7 @@ const path = require("path");
 const programDir = path.join(__dirname, "..", "..", "programs", "multisig");
 const idlDir = path.join(__dirname, "idl");
 const sdkDir = path.join(__dirname, "src", "generated");
-const binaryInstallDir = path.join(__dirname, ".crates");
+const binaryInstallDir = path.join(__dirname, "..", "..", ".crates");
 
 module.exports = {
   idlGenerator: "anchor",
