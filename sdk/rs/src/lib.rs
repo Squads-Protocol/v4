e5dfcee142c16d9255c92157d092d79568179b19
Vladimir Guguiev <1524432+vovacodes@users.noreply.github.com>
2023-09-09 13:17:33 +0200

    feat(sdk-rs): add get_multisig

--- sdk/rs/src/lib.rs
+++ sdk/rs/src/lib.rs
@@ -1,8 +1,24 @@
-// Export current sdk types for downstream users building with a different sdk version
+// Re-export dependencies for convenience
+pub use solana_client;
+
+pub use squads_multisig_program;
 pub use squads_multisig_program::anchor_lang;
 pub use squads_multisig_program::anchor_lang::solana_program;
-// Export the program types
-pub use squads_multisig_program;
+
+pub mod error {
+    use thiserror::Error;
+
+    #[derive(Debug, Error)]
+    pub enum ClientError {
+        #[error(transparent)]
+        Client(#[from] solana_client::client_error::ClientError),
+
+        #[error("Failed to deserialize account data")]
+        DeserializationError,
+    }
+}
+
+pub type ClientResult<T> = Result<T, error::ClientError>;
 
 pub mod state {
     pub use squads_multisig_program::state::{
@@ -14,14 +30,13 @@ pub mod state {
 
 pub mod cpi {
     use squads_multisig_program::anchor_lang::prelude::{CpiContext, Pubkey, Result};
-    use squads_multisig_program::Member;
-
     pub use squads_multisig_program::cpi::accounts::{
         BatchAddTransaction, BatchCreate, BatchExecuteTransaction, ConfigTransactionCreate,
         ConfigTransactionExecute, MultisigAddSpendingLimit, MultisigConfig, MultisigCreate,
         MultisigRemoveSpendingLimit, ProposalActivate, ProposalCreate, ProposalVote,
         SpendingLimitUse, VaultTransactionCreate, VaultTransactionExecute,
     };
+    use squads_multisig_program::Member;
 
     pub fn create_multisig<'info>(
         ctx: CpiContext<'_, '_, '_, 'info, MultisigCreate<'info>>,
@@ -45,14 +60,33 @@ pub mod cpi {
 }
 
 pub mod client {
-    use squads_multisig_program::anchor_lang::prelude::Pubkey;
-    use squads_multisig_program::anchor_lang::{
-        solana_program::instruction::Instruction, InstructionData, ToAccountMetas,
-    };
+    use solana_client::nonblocking::rpc_client::RpcClient;
 
     pub use squads_multisig_program::accounts::ConfigTransactionCreate as ConfigTransactionCreateAccounts;
     pub use squads_multisig_program::instruction::ConfigTransactionCreate as ConfigTransactionCreateData;
     pub use squads_multisig_program::instructions::ConfigTransactionCreateArgs;
+    use squads_multisig_program::Multisig;
+
+    use crate::anchor_lang::prelude::Pubkey;
+    use crate::anchor_lang::AnchorDeserialize;
+    use crate::anchor_lang::{
+        solana_program::instruction::Instruction, InstructionData, ToAccountMetas,
+    };
+    use crate::error::ClientError;
+    use crate::ClientResult;
+
+    /// Gets a `Multisig` account from the chain.
+    pub async fn get_multisig(
+        rpc_client: &RpcClient,
+        multisig_key: &Pubkey,
+    ) -> ClientResult<Multisig> {
+        let multisig_account = rpc_client.get_account(multisig_key).await?;
+
+        let multisig = Multisig::try_from_slice(&multisig_account.data)
+            .map_err(|_| ClientError::DeserializationError)?;
+
+        Ok(multisig)
+    }
 
     /// Creates a new multisig config transaction.
     /// Example:
