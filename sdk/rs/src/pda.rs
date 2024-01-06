992296521b92fa595d9be85e0784d0395edbf086
Vladimir Guguiev <1524432+vovacodes@users.noreply.github.com>
2024-01-06 22:18:07 +0100

    feat(sdk-rs): add get `get_program_config_pda`

--- sdk/rs/src/pda.rs
+++ sdk/rs/src/pda.rs
@@ -1,9 +1,16 @@
 use squads_multisig_program::{
-    SEED_EPHEMERAL_SIGNER, SEED_MULTISIG, SEED_PREFIX, SEED_PROPOSAL, SEED_SPENDING_LIMIT,
-    SEED_TRANSACTION, SEED_VAULT,
+    SEED_EPHEMERAL_SIGNER, SEED_MULTISIG, SEED_PREFIX, SEED_PROGRAM_CONFIG, SEED_PROPOSAL,
+    SEED_SPENDING_LIMIT, SEED_TRANSACTION, SEED_VAULT,
 };
 
 use crate::solana_program::pubkey::Pubkey;
+
+pub fn get_program_config_pda(program_id: Option<&Pubkey>) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[SEED_PREFIX, SEED_PROGRAM_CONFIG],
+        program_id.unwrap_or(&squads_multisig_program::ID),
+    )
+}
 
 pub fn get_multisig_pda(create_key: &Pubkey, program_id: Option<&Pubkey>) -> (Pubkey, u8) {
     Pubkey::find_program_address(
