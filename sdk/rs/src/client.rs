236ae08b46733230c3215fa0064ca5077e1adb9c
Valentin Madrid <valentin@incility.com>
2024-01-10 23:47:02 +0100

    change transaction mutability

--- sdk/rs/src/client.rs
+++ sdk/rs/src/client.rs
@@ -641,7 +641,7 @@ pub mod utils {
                 AccountMeta::new(self.multisig, false),
                 AccountMeta::new_readonly(self.member, true),
                 AccountMeta::new(self.proposal, false),
-                AccountMeta::new_readonly(self.transaction, false),
+                AccountMeta::new(self.transaction, false),
                 if let Some(rent_payer) = self.rent_payer {
                     AccountMeta::new(rent_payer, true)
                 } else {
