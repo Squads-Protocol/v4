d430f5321fa34f5c486936eb9aa84f972cd9c9fb
Joey Meere <100378695+joeymeere@users.noreply.github.com>
2024-05-01 09:53:28 -0400

    feat(ix): add multisigChangeThreshold to instructions

---
+++ sdk/multisig/src/instructions/multisigChangeThreshold.ts
@@ -0,0 +1,37 @@
+import { PublicKey } from "@solana/web3.js";
+import {
+  createMultisigChangeThresholdInstruction,
+  PROGRAM_ID,
+} from "../generated";
+
+export function multisigChangeThreshold({
+  multisigPda,
+  configAuthority,
+  rentPayer,
+  newThreshold,
+  memo,
+  programId = PROGRAM_ID,
+}: {
+  multisigPda: PublicKey;
+  spendingLimit: PublicKey;
+  configAuthority: PublicKey;
+  rentPayer: PublicKey;
+  newThreshold: number;
+  memo?: string;
+  programId?: PublicKey;
+}) {
+  return createMultisigChangeThresholdInstruction(
+    {
+      multisig: multisigPda,
+      configAuthority,
+      rentPayer,
+    },
+    {
+      args: {
+        newThreshold,
+        memo: memo ?? null,
+      },
+    },
+    programId
+  );
+}
