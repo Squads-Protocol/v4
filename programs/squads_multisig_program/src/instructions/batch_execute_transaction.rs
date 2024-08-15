2ba1ee82671f9ef2e217743a9c17b1073eeef067
Sean <sean@organgrindermedia.com>
2024-08-15 11:30:39 -0400

    pass vault seed slice directly

--- programs/squads_multisig_program/src/instructions/batch_execute_transaction.rs
+++ programs/squads_multisig_program/src/instructions/batch_execute_transaction.rs
@@ -150,10 +150,7 @@ impl BatchExecuteTransaction<'_> {
 
         // Execute the transaction message instructions one-by-one.
         executable_message.execute_message(
-            &vault_seeds
-                .iter()
-                .map(|seed| seed.to_vec())
-                .collect::<Vec<Vec<u8>>>(),
+            vault_seeds,
             &ephemeral_signer_seeds,
             protected_accounts,
         )?;
