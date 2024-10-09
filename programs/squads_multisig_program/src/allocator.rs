821e9a876d9945add4e389615c40ed872ddf9067
Iceomatic <89707822+iceomatic@users.noreply.github.com>
2024-10-09 10:02:46 +0200

    remove: testing code from allocator

--- programs/squads_multisig_program/src/allocator.rs
+++ programs/squads_multisig_program/src/allocator.rs
@@ -103,27 +103,22 @@ struct BumpAllocator;
 unsafe impl std::alloc::GlobalAlloc for BumpAllocator {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if layout.size() == isize::MAX as usize - 0x42 {
-            // Return test value
-            0x42 as *mut u8
-        } else {
-            const POS_PTR: *mut usize = HEAP_START_ADDRESS as *mut usize;
-            const TOP_ADDRESS: usize = HEAP_START_ADDRESS as usize + HEAP_LENGTH;
-            const BOTTOM_ADDRESS: usize = HEAP_START_ADDRESS as usize + size_of::<*mut u8>();
-            let mut pos = *POS_PTR;
-            if pos == 0 {
-                // First time, set starting position to bottom address
-                pos = BOTTOM_ADDRESS;
-            }
-            // Align the position upwards
-            pos = (pos + layout.align() - 1) & !(layout.align() - 1);
-            let next_pos = pos.saturating_add(layout.size());
-            if next_pos > TOP_ADDRESS {
-                return null_mut();
-            }
-            *POS_PTR = next_pos;
-            pos as *mut u8
+        const POS_PTR: *mut usize = HEAP_START_ADDRESS as *mut usize;
+        const TOP_ADDRESS: usize = HEAP_START_ADDRESS as usize + HEAP_LENGTH;
+        const BOTTOM_ADDRESS: usize = HEAP_START_ADDRESS as usize + size_of::<*mut u8>();
+        let mut pos = *POS_PTR;
+        if pos == 0 {
+            // First time, set starting position to bottom address
+            pos = BOTTOM_ADDRESS;
         }
+        // Align the position upwards
+        pos = (pos + layout.align() - 1) & !(layout.align() - 1);
+        let next_pos = pos.saturating_add(layout.size());
+        if next_pos > TOP_ADDRESS {
+            return null_mut();
+        }
+        *POS_PTR = next_pos;
+        pos as *mut u8
     }
 
     #[inline]
